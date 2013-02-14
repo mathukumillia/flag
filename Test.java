5e2175a413713e44b8d66cf8588b9871ac1e8fb7
Anirudh <mathukumillia@s.dcsdk12.org>
2013-02-14 07:52:39 -0700

    created point and test flag

---
+++ Test.java
@@ -0,0 +1,15 @@
+/**
+ *Test
+ *
+ * Creates points
+ *
+ * 
+ */
+
+public class Test {
+	public static void main(String[] args) {
+		Point p = new Point(3,6);
+
+		System.out.println(p.x);
+	}
+}
---
+++ point.java
@@ -0,0 +1,22 @@
+/**
+ *  Point
+ *  	represents a point in 2D space
+ *
+ * 
+ */
+
+public class Point {
+	int x, y;
+
+	/**
+	 *  Constructor
+	 *
+	 *  @param - x - x coordinate of point
+	 *  @param - y - y coordinate of point
+	 * 	
+	 */
+	public Point(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+}
